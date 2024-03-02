b8ef2c3239cbc6b00938fb0284ee3d24c66d840b
touchtak <159424108+touchtak@users.noreply.github.com>
2024-03-02 11:13:18 +0000

    finish

--- sample_app/app/controllers/lists_controller.rb
+++ sample_app/app/controllers/lists_controller.rb
@@ -28,8 +28,8 @@ class ListsController < ApplicationController
   end
 
   private
-  
+
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 end
